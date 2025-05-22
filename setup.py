0e0444d109ab1611cc02c0f3f95c51375773d52d
unknown <adhithiyan12899@gmail.com>
2025-05-22 04:16:23 +0300

    Initial commit of Blender wheel generator add-on

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wheelgen",
+    version="1.0",
+    author="Your Name",
+    description="Blender add-on for generating wheels",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+)
